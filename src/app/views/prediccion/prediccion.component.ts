e5b20fec842b694a0ad16bc9d1ecba9714bba6fb
IamSebx <108105889+IamSebx@users.noreply.github.com>
2025-04-18 12:40:36 -0500

    Create component

---
+++ src/app/views/prediccion/prediccion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prediccion',
+  imports: [],
+  templateUrl: './prediccion.component.html',
+  styleUrl: './prediccion.component.css'
+})
+export class PrediccionComponent {
+
+}
