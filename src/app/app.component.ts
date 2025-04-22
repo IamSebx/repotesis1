d98655655876b96435fcb653029a8a7dd2cf2f06
IamSebx <108105889+IamSebx@users.noreply.github.com>
2025-04-22 09:33:57 -0500

    Avance 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BienvenidaComponent } from './views/bienvenida/bienvenida.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule, BienvenidaComponent],
+  imports: [RouterOutlet, CommonModule, FormsModule, BienvenidaComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
