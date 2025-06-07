b0287f0f0e5ec23fbbde4b2d1e08bfca85ee0d1b
Alejandro Toshiro Escobedo Correa <toshiro.escobedo@intediperu.com>
2025-06-07 09:44:50 -0500

    add apiUrls

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'http://54.198.254.35',
-    'http://ec2-54-198-254-35.compute-1.amazonaws.com'
+    'https://detector-tbc-api.strategyec.com',
+    'https://detector-tbc-api.strategyec.com'
   ],
   defaultApiUrl: 'https://detector-tbc-api.strategyec.com',
   apiTimeout: 30000
