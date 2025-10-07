37f83371d1f50e54ca374cd6298ebf4598efae8d
IamSebx <brunose0806@gmail.com>
2025-10-07 17:39:20 -0500

    Modificación del Proyecto LearnTB

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BienvenidaComponent } from './views/bienvenida/bienvenida.component';
 import { PrediccionComponent } from './views/prediccion/prediccion.component';
+import { ConceptosBasicosComponent } from './views/conceptos-basicos/conceptos-basicos.component';
+import { TratamientoComponent } from './views/tratamiento/tratamiento.component';
+import { PrevencionComponent } from './views/prevencion/prevencion.component';
 
 export const routes: Routes = [
     {
@@ -10,8 +13,20 @@ export const routes: Routes = [
         component: BienvenidaComponent,
     },
     {   
+        path: 'conceptos-basicos',
+        component: ConceptosBasicosComponent,
+    },
+    {   
+        path: 'prevencion',
+        component: PrevencionComponent,
+    },
+    {   
         path: 'diagnostico',
         component: PrediccionComponent,
+    },
+    {   
+        path: 'tratamiento',
+        component: TratamientoComponent,
     },
     {
         path: '**', redirectTo: '/'
---
+++ src/app/views/prevencion/prevencion.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-prevencion',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './prevencion.component.html',
+  styleUrl: './prevencion.component.css'
+})
+export class PrevencionComponent {
+
+}
